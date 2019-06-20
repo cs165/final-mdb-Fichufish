29a6625c0ac9cb9b2d79f8c1d7b598dbd82522ac
Fichufish <39399950+Fichufish@users.noreply.github.com>
2019-06-21 04:11:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ const ObjectID = require('mongodb').ObjectID;
 async function main() {
   const DATABASE_NAME = 'cs193x-db';
   //const MONGO_URL = `mongodb://localhost:27017/${DATABASE_NAME}`;
-  const MONGO_URL = `mongodb+srv://Owo:1234@cluster0-tglrc.mongodb.net/test?retryWrites=true&w=majority
-/${DATABASE_NAME}`;
+  const MONGO_URL = `mongodb+srv://Owo:1234@cluster0-tglrc.mongodb.net/test?retryWrites=true&w=majority/${DATABASE_NAME}`;
   
   // The "process.env.MONGODB_URI" is needed to work with Heroku.
   db = await MongoClient.connect(process.env.MONGODB_URI || MONGO_URL);
